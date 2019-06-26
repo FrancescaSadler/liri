19ecfc68171f966705d46fc4abe03fc5a907c2c7
unknown <fsadler9@gmail.com>
2019-06-25 18:33:50 -0600

    update

--- liri.js
+++ liri.js
@@ -18,6 +18,7 @@ switch (operator) {
         doWhat();
         break;
     case "movie-this":
+        movieThis();
         console.log("movie works")
         break;
     case "spotify-this-song":
@@ -117,6 +118,43 @@ function song() {
 //    * Plot of the movie.
 //    * Actors in the movie.
 
+
+function movieThis () {
+    var queryUrl = "http://www.omdbapi.com/?t=" + action + "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+       function (response) {
+           console.log(response.data.Title);
+           console.log(response.data.Year);
+           console.log(response.data.Genre);
+           console.log(response.data.Actors);
+           console.log(response.data.Plot);
+           console.log(response.data.Country);
+           console.log(response.data[1].Value);
+
+       })
+
+       .catch(function(error) {
+            if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an object that comes back with details pertaining to the error that occurred.
+              console.log(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log("Error", error.message);
+            }
+            console.log(error.config);
+          });
+
+      
+}
 // var queryUrl = "http://www.omdbapi.com/?t=" + action + "&y=&plot=short&apikey=trilogy";
 // console.log(queryUrl);
 
